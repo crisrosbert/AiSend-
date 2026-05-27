a91b3a2c77284772895b747a3e2e6475ec525cbf
crisrosbert <crisrosbert@gmail.com>
2026-05-27 15:28:28 +0530

    Delete src/app/slug/dashboard directory

--- src/app/slug/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-// src/app/[slug]/dashboard/page.tsx
-// This file makes /[slug]/dashboard work
-// It simply re-exports the existing dashboard page
-
-export { default } from "@/app/(dashboard)/dashboard/page";
